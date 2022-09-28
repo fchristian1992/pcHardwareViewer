96137c8162f5122a650145f4c89e832adbd6f0c9
fchristian1992 <fchristian@madisoncollege.edu>
2022-09-28 12:15:20 -0500

    Added Project Plan, updated pom.xml, and added the config file for hibernate

---
+++ ProjectPlan.md
@@ -0,0 +1,18 @@
+# Project Plan
+
+### Week 4
+- Create MySQL database
+- Create javabeans for User and Hardware and implement Hibernate
+- Weekly reflection
+
+### Week 5
+- Sign in jsp
+- sign up jsp
+- Weekly reflection
+
+### Week 6
+- parts list jsp
+- edit list jsp
+- Weekly reflection
+
+To be updated....
---
+++ src/main/java/entity/User.java
@@ -0,0 +1,30 @@
+package entity;
+
+class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
